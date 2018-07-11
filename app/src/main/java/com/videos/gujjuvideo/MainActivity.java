aaaf265ba90edc7a977240cf1afb05e9857c6d70
MehulBhundiya <mehul.bhundiya1991@gmail.com>
2018-07-11 16:16:18 +0530

    Initial commit

---
+++ app/src/main/java/com/videos/gujjuvideo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.videos.gujjuvideo;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.content_recyclerview)
+    RecyclerView contentRecyclerview;
+    @BindView(R.id.content_main)
+    LinearLayout contentMain;
+    @BindView(R.id.nav_view)
+    NavigationView navView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
